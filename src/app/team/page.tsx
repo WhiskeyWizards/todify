4b6f0e22df4ef4fdd9f44188cb5f600cf3c27dd6
shivamS21 <2019ucs0100@iitjammu.ac.in>
2024-07-20 23:12:46 +0530

    Migrated to App-router

---
+++ src/app/team/page.tsx
@@ -0,0 +1,10 @@
+export default function Music() {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen gap-3">
+            <h1 className="border-solid">Full Stack Developer Team</h1>
+            <span>Ankush</span>
+            <span>Manik</span>
+            <span>Shivam</span>
+        </div>
+    )
+}
